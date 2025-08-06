6e194e7bb88f9c5a073e03181f9f625765c146d4
Michel Assi-Bodje <michelassibodje@gmail.com>
2025-08-06 16:22:14 -0400

    more tweaks

--- app/build.bat
+++ app/build.bat
@@ -4,7 +4,6 @@ pyinstaller ^
   --onefile ^
   --noconsole ^
   --name AdminHub ^
-  --distpath dist ^
   --add-data "src;app/src" ^
   --add-data "web;app/web" ^
   --add-data "templates;app/templates" ^
